b7c2366f5adcdd862ed33ba1f9fbde94a27d9d3e
Pratiksha Menpara <pxa1772@gmail.com>
2026-03-07 10:56:43 +0530

    fix: issue of the img

--- rewardSystem.js
+++ rewardSystem.js
@@ -2,8 +2,30 @@ const rewardImageContainer = document.getElementById("rewardImageContainer");
 const downloadBtn = document.getElementById("downloadBtn");
 const continueBtn = document.getElementById("continueBtn");
 
+let currentLevelNumber = 1;
+
+function downloadImage(levelNumber) {
+  fetch(`images/img${levelNumber}.png`)
+    .then(response => response.blob())
+    .then(blob => {
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = `img${levelNumber}.png`;
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+      URL.revokeObjectURL(url);
+    })
+    .catch(err => {
+      console.error('Download failed:', err);
+      window.open(`images/img${levelNumber}.png`, '_blank');
+    });
+}
+
 function showReward(levelNumber) {
   showScreen("reward");
+  currentLevelNumber = levelNumber;
 
   rewardImageContainer.innerHTML = "";
 
@@ -18,9 +40,9 @@ function showReward(levelNumber) {
   wrapper.appendChild(img);
   rewardImageContainer.appendChild(wrapper);
 
-  downloadBtn.href = `images/img${levelNumber}.png`;
-  downloadBtn.download = `img${levelNumber}.png`;
-  downloadBtn.target = "_blank";
+  downloadBtn.onclick = () => downloadImage(levelNumber);
+  downloadBtn.href = "#";
+  downloadBtn.style.pointerEvents = "auto";
 
   continueBtn.onclick = () => showScreen("map");
 }
